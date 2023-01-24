8dcb57c298162800000ac6b7630268cbab472e0f
TheRootSixtyFour <TheRootSixtyFour@gmail.com>
2023-01-24 11:17:25 +0100

    add a value for rgba sliders. include all functionality for coloring the desired DIV element

--- color-slider.js
+++ color-slider.js
@@ -1,10 +1,12 @@
 const redSlider = document.getElementById("red-slider");
 const blueSlider = document.getElementById("blue-slider");
 const greenSlider = document.getElementById("green-slider");
+const aSlider = document.getElementById("a-slider");
 
 const redValue = document.getElementById("red-value");
 const blueValue = document.getElementById("blue-value");
 const greenValue = document.getElementById("green-value");
+const aValue = document.getElementById("a-value");
 
 const colorMe = document.getElementById("color-me");
 const bodyEl = document.getElementsByTagName("body");
@@ -12,20 +14,24 @@ const bodyEl = document.getElementsByTagName("body");
 redValue.innerText = redSlider.value;
 blueValue.innerText = blueSlider.value;
 greenValue.innerText = greenSlider.value;
+aValue.innerText = aSlider.value;
 
 let userRed = "0";
 let userBlue = "0";
 let userGreen= "0";
-colorMe.style.backgroundColor = `rgb(${userRed}, ${userGreen}, ${userBlue})`;
+let userA = "1"
+
+colorDiv(); 
 
 function colorDiv() {
-    colorMe.style.backgroundColor = `rgb(${userRed}, ${userGreen}, ${userBlue})`
+    colorMe.style.backgroundColor = `rgba(${userRed}, ${userGreen}, ${userBlue}, ${userA})`
 }
 
 redSlider.oninput = function() {
     redValue.innerText = this.value;
     userRed = this.value;
-    colorDiv();}
+    colorDiv();
+}
 
 greenSlider.oninput = function() {
     greenValue.innerText = this.value;
@@ -36,5 +42,11 @@ greenSlider.oninput = function() {
 blueSlider.oninput = function() {
     blueValue.innerText = this.value;
     userBlue = this.value;
+    colorDiv();
+}
+
+aSlider.oninput = function() {
+    aValue.innerText = this.value;
+    userA = this.value;
     colorDiv();
 }
